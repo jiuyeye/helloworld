e24c70fb85ceb00d3bac7233e6f2b955e7216c1e
lcq <lcq0202@163.com>
2016-11-16 00:09:17 +0800

    router

--- example/router.js
+++ example/router.js
@@ -5,12 +5,25 @@ var http=require('http'),
 		var pathname=url.parse(req.url).pathname;
 
 		if(pathname === '/'){
-			res.writeHead(301,{
-				'Location':'http://www.baidu.com'
+			res.writeHead(200,{
+				'Content-Type':'text/plain'
 			});
 			res.end('home page');
+		}else if(pathname === '/about'){
+			res.writeHead(200,{
+				'Content-Type':'text/plain'
+			});
+			res.end('home page');
+		}else if(pathname === '/redirect'){
+			res.writeHead(301,{
+				'Location':'/'
+			});
+			res.end('home page');
+		}else{
+			res.writeHead(400,{
+				'Content-Type':'text/plain'
+			});
+			res.end('not page');
 		}
-		
-		
 	}).listen(2000,"127.0.0.1");
 	console.log('Server running an http://127.0.0.1:2000/')
