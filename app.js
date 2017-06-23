4ed6f15236819cb26c910a0d671a99e4b68f3509
lcq0202 <lcq0202@163.com>
2017-06-23 18:55:29 +0800

    node 调用函数

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var http=require('http');
 	http.createServer(function(req,res){
 		res.writeHead(200,{'Content-Type':'text/plain'});
-		res.end('Hello Wdddorld\n');
+		if(req.url!=='/favicon.ico'){
+			console.log(1);
+			res.end('Hello Wdddorld\n');
+		}
 	}).listen(3300,"127.0.0.1");
 	console.log('Server running an http://127.0.0.1:3300/')
